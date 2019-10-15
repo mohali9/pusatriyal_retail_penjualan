74ef4e31046e2a713b171c2f2eb93cbb27eb8bf5
mohali9 <mohali.ilkom@gmail.com>
2019-10-15 13:54:50 +0700

    rapikan field

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Penjualan(),
+      routes: <String, WidgetBuilder>{
+        '/Penjualan': (BuildContext context) => new Penjualan(),
+        '/Pemesananan': (BuildContext context) => new Pemesananan(),
+      },
     );
   }
 }
@@ -47,17 +51,20 @@ class _PenjualanState extends State<Penjualan> {
               ),
             ),
             ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
+                title: Text('Penjualan'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Penjualan()),
+                  );
+                }),
             ListTile(
-              title: Text('Item 2'),
+              title: Text('Pemesanan'),
               onTap: () {
-                // Update the state of the app.
-                // ...
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Pemesananan()),
+                );
               },
             ),
           ],
@@ -74,7 +81,7 @@ class _PenjualanState extends State<Penjualan> {
                   padding: const EdgeInsets.all(15.0),
                   child: new Text(
                     "Nota",
-                    style: new TextStyle(fontSize: 20.0),
+                    style: new TextStyle(fontSize: 18.0),
                   ),
                 ),
                 new ListTile(
@@ -90,9 +97,12 @@ class _PenjualanState extends State<Penjualan> {
                     ),
                   ),
                 ),
-                new Text(
-                  "Tanggal",
-                  style: new TextStyle(fontSize: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Tanggal",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
                 ),
                 new ListTile(
                   title: new TextField(
@@ -107,9 +117,12 @@ class _PenjualanState extends State<Penjualan> {
                     ),
                   ),
                 ),
-                new Text(
-                  "Tipe Penjualan",
-                  style: new TextStyle(fontSize: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Tipe Penjualan",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
                 ),
                 new ListTile(
                   title: new TextField(
@@ -124,9 +137,12 @@ class _PenjualanState extends State<Penjualan> {
                     ),
                   ),
                 ),
-                new Text(
-                  "Gudang",
-                  style: new TextStyle(fontSize: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Gudang",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
                 ),
                 new ListTile(
                   title: new TextField(
@@ -141,9 +157,12 @@ class _PenjualanState extends State<Penjualan> {
                     ),
                   ),
                 ),
-                new Text(
-                  "Metode Penjualan",
-                  style: new TextStyle(fontSize: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Metode Penjualan",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
                 ),
                 new ListTile(
                   title: new TextField(
@@ -158,9 +177,12 @@ class _PenjualanState extends State<Penjualan> {
                     ),
                   ),
                 ),
-                new Text(
-                  "Metode Pembayaran",
-                  style: new TextStyle(fontSize: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Metode Pembayaran",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
                 ),
                 new ListTile(
                   title: new TextField(
@@ -175,9 +197,12 @@ class _PenjualanState extends State<Penjualan> {
                     ),
                   ),
                 ),
-                new Text(
-                  "Pilih Akun",
-                  style: new TextStyle(fontSize: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Pilih Akun",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
                 ),
                 new ListTile(
                   title: new TextField(
@@ -192,9 +217,12 @@ class _PenjualanState extends State<Penjualan> {
                     ),
                   ),
                 ),
-                new Text(
-                  "Customer",
-                  style: new TextStyle(fontSize: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Customer",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
                 ),
                 new ListTile(
                   title: new TextField(
@@ -209,9 +237,12 @@ class _PenjualanState extends State<Penjualan> {
                     ),
                   ),
                 ),
-                new Text(
-                  "Alamat Cust",
-                  style: new TextStyle(fontSize: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Alamat Cust",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
                 ),
                 new ListTile(
                   title: new TextField(
@@ -226,9 +257,12 @@ class _PenjualanState extends State<Penjualan> {
                     ),
                   ),
                 ),
-                new Text(
-                  "Keterangan",
-                  style: new TextStyle(fontSize: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Keterangan",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
                 ),
                 new ListTile(
                   title: new TextField(
@@ -243,9 +277,268 @@ class _PenjualanState extends State<Penjualan> {
                     ),
                   ),
                 ),
-                new Text(
-                  "Pilih Rate",
-                  style: new TextStyle(fontSize: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Pilih Rate",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Pilih Rate',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// =============== PEMESANAN =============
+
+class Pemesananan extends StatefulWidget {
+  @override
+  _PemesanananState createState() => _PemesanananState();
+}
+
+class _PemesanananState extends State<Pemesananan> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        // leading: new Icon(Icons.home),
+        title: new Center(
+          child: new Text(
+            "Pemesanan",
+            style: new TextStyle(color: Colors.red),
+          ),
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          new Container(
+            padding: new EdgeInsets.all(10.0),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Nota",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Nota',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Tanggal",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Tanggal',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Tipe Penjualan",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Tipe',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Gudang",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Gudang',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Metode Penjualan",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Metode Penjualan',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Metode Pembayaran",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Metode Pembayaran',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Pilih Akun",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Pilih Akun',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Customer",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Customer',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Alamat Cust",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Alamat Cust',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Keterangan",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                new ListTile(
+                  title: new TextField(
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      hintText: 'Keterangan',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text(
+                    "Pilih Rate",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
                 ),
                 new ListTile(
                   title: new TextField(
